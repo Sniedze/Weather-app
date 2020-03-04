ae5e2ba50efc1e5ee9726b92fa9840485aa18a6d
Ulrika Sniedze <u.sniedze@gmail.com>
2020-03-04 10:00:45 +0100

    Adds state to Search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Search = props => {
-  const { handleInput, handleSubmit, city, country } = props;
-  return (
-    <div className="search_container">
-      <form method="get" className="search_container">
-        <input
-          onChange={handleInput}
-          type="text"
-          id="city"
-          name="city"
-          defaultValue={city}
-          placeholder="City..."
-        />
-        <input
-          onChange={handleInput}
-          type="text"
-          name="country"
-          defaultValue={country}
-          placeholder="Country..."
-        />
+export default class Search extends Component {
+  state = {
+    city: "",
+    country: ""
+  };
 
-        <button onClick={handleSubmit}>Get Weather</button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    const { handleInput, handleSubmit, city, country } = this.props;
+    return (
+      <div className="search_container">
+        <form method="get" className="search_container">
+          <input
+            onChange={handleInput}
+            type="text"
+            id="city"
+            name="city"
+            defaultValue={city}
+            placeholder="City..."
+          />
+          <input
+            onChange={handleInput}
+            type="text"
+            name="country"
+            defaultValue={country}
+            placeholder="Country..."
+          />
 
-export default Search;
+          <button onClick={handleSubmit}>Get Weather</button>
+        </form>
+      </div>
+    );
+  }
+}
